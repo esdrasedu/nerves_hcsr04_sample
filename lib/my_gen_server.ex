d8e325ef925c395416709b36279e733ed30d783e
Esdras Eduardo <esdrasedu@gmail.com>
2018-06-25 17:01:27 -0300

    add geserver

---
+++ lib/my_gen_server.ex
@@ -0,0 +1,16 @@
+defmodule MyGenServer do
+  use NervesHcsr04
+
+  def listen({:ok, d, _port, {e, t}}) do
+    IO.puts("Success on MyGenServer")
+    IO.puts("Echo: #{e}, Trig: #{t}\n")
+    IO.puts("Distance: #{d}\n")
+  end
+
+  def listen({:error, code_error, _port, {e, t}}) do
+    IO.puts("Error on MyGenServer")
+    IO.puts("Echo: #{e}, Trig: #{t}\n")
+    IO.puts("Error: #{code_error}\n")
+  end
+
+end
